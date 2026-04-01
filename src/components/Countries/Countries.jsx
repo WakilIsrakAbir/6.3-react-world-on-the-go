62ad387696b4b5730215a617edd3f6fe5ceaf83d
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-04-01 10:04:27 +0600

    lift up state in array

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -9,6 +9,8 @@ const Countries = ({ countriesPromise }) => {
 
   const handleVisitedCountries = (country) => {
     console.log("handle visited country clicked", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
   }
 
   const countriesData = use(countriesPromise);
@@ -16,10 +18,18 @@ const Countries = ({ countriesPromise }) => {
   return (
     <div>
       <h1>In The Counteies : {countries.length} </h1>
-      <h3>Total Country Visited: </h3>
+      <h3>Total Country Visited: {visitedCountries.length} </h3>
+      <ol>
+        {
+          visitedCountries.map(country => <li
+          key={country.cca3.cca3}
+          > {country.name.common} </li>)
+        }
+      </ol>
       <div className="countries">
         {countries.map((country) => (
           <Country
+          key={country.cca3.cca3}
             country={country}
             handleVisitedCountries = {handleVisitedCountries}
           ></Country>
